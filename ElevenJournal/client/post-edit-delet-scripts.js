6aa55e29a8203728db26b2e973194eaacb346235
KaliCF <kalifenimore@gmail.com>
2021-08-31 10:25:21 -0400

    8/31/21

--- ElevenJournal/client/post-edit-delet-scripts.js
+++ ElevenJournal/client/post-edit-delet-scripts.js
@@ -1,14 +1,101 @@
 //POST JOURNAL:
 function postJournal() {
     console.log(`postJournal Function Called`)
+    const accessToken = localStorage.getItem('sessionToken')
+    let title = document.getElementById('title').value;
+    let date = document.getElementById('date').value;
+    let entry = document.getElementById('entry').value;
+
+    let newEntry = {
+        journal: {
+            title: title,
+            date: date,
+            entry: entry
+        }
+    }
+
+    fetch(`http://localhost:3000/journal/vreate`, {
+        method: "POST",
+        headers: new Headers({
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${accessToken}`
+        }),
+        body: JSON.stringify(newEntry)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            displayMine()
+        })
+        .catch(err => {
+            console.error(err)
+        })
 }
 
 //UPDATE JOURNAL:
 function editJournal() {
-    console.log(`editJournal Function Called`)
+    console.log(postId);
+
+    const fetch_url = `http://localhost:3000/journal/update/${postId}`;
+    const accessToken = localStorage.getItem('SessionToken');
+
+    let card = document.getElementById(postId);
+    let input = document.getElementById('input');
+
+    if (card.childNodes.length < 2) {
+        card.appendChild(input);
+        input.setAttribute('type', 'text');
+        input.setAttribute('id', 'updateEntry');
+        input.setAttribute('placeholder', 'Edit your Journal entry');
+    } else {
+        let updated = document.getElementById('updateEntry').value;
+        let newEntry = {
+            journal: {
+                entry: updated
+            }
+        }
+
+        fetch(fetch_url, {
+            method: "PUT",
+            headers: new Headers({
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${accessToken}`
+            }),
+            body: JSON.stringify(newEntry)
+        })
+            .then(repsonse => response.json())
+            .then(data => {
+                console.log(data);
+                displayMine();
+            })
+            .catch(err => {
+                console.error(err)
+            })
+        card.removeChild(card.lastChild)
+    }
 }
 
 //DELETE JOURNAL:
 function deleteJournal() {
     console.log(`deleteJournal Function Called`)
+    console.log(postId);
+
+    const fetch_url = `http://localhost:3000/journal/delete/${postId}`;
+    const accessToken = localStorage.getItem('SessionToken');
+
+    fetch(fetch_url, {
+        method: "DELETE",
+        headers: new Headers({
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${accessToken}`
+        })
+    })
+        .then(reponse => reponse.json())
+        .then(data => {
+            console.log(data);
+            displayMine();
+        })
+        .catch(err => {
+            console.error(err)
+        })
 }
